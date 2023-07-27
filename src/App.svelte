058a64f3d3225047e5c741f857654bfb71930878
Rafael Alexis Pinto Florez <rafael.pinto@raddarstudios.com>
2023-07-27 17:36:23 -0500

    init proyect

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+    import {onMount} from "svelte";
+	import TimeLine from './Components/TimeLine.svelte';
+	import Sidebar from './Components/Sidebar.svelte';
+    import Footer from "./Components/Footer.svelte";
+    import Header from "./Components/Header.svelte";
+    import Main from './Components/Main.svelte';
+    
+    let data = {};
+    const API = "https://kittygram-api.now.sh/"
+
+    onMount (async ()=> {
+        const response = await fetch(API)
+        data= await response.json();
+    });
+
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap');
+
+
+    :global(body){
+        background-color:  antiquewhite
+        color rgba(38,38,38,0.7);
+        font-family:"lato", sans-serif; 
+        margin: 0;
+        padding: 0; 
+        }   
+    :global(h1,h2,h3)  {
+        margin: 0;
+        padding: 0;
+    }  
+</style>
+
+<Header/>  
+<Main>
+<TimeLine posts={data.posts}/>
+<Sidebar  {...data.user}/>
+<Footer />
+</Main>
---
+++ src/Components/Footer.svelte
@@ -0,0 +1,20 @@
+<style>
+    .Footer {
+      font-size: 11px;
+      letter-spacing: 1px;
+      font-weight: normal;
+      color: #c7c7c7;
+    }
+  </style>
+  
+
+<div class =  "Footer">
+    <div class =  "Footer-container">
+        <div class =  "Footer-copy"></div>
+        <h1> © 2020 PUGSTAGRAM FROM PLATZY</h1>
+        <p>Este es el contenido del Footer.</p>
+
+    </div>
+    
+    </div>
+    
